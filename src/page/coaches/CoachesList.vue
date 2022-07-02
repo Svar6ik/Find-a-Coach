fb14955fdb397d966f8f3a974ecede43ce8b0d55
Svar6ik <104255066+Svar6ik@users.noreply.github.com>
2022-07-02 14:52:05 +0600

    Coaches List and List Items, Adding a Couple of Base Components

--- src/page/coaches/CoachesList.vue
+++ src/page/coaches/CoachesList.vue
@@ -3,12 +3,35 @@
     FILTER
   </header>
   <section>
-    COACHES LIST
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item v-for="coach in filtredCoaches" :key="coach.id" :id="coach.id" :firstName="coach.firstName"
+          :lastName="coach.lastName" :areas="coach.areas" :rate="coach.hourlyRate"></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
- export default {}
+import CoachItem from '../../components/coaches/CoachItem.vue';
+export default {
+  components: {
+    CoachItem
+  },
+  computed: {
+    filtredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hascoaches'];
+    }
+  }
+}
 </script>
 
 <style>
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "vuex";
+
+import coachesModule from './coaches/index.js';
+import requestsModule from './requests/index.js';
+
+const store = createStore({
+  modules: {
+    coaches: coachesModule,
+    requests: requestsModule
+  },
+  state() {
+    return {};
+  },
+});
+
+export default store;
